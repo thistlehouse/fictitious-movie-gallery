59f51e173c16b4efd639a5a4d02765b9e405cad3
thistlehouse <fernando2802@gmail.com>
2024-03-11 00:12:49 -0300

    using year fom movie

--- src/movie-gallery.ui/src/app/movies/details/[id]/page.tsx
+++ src/movie-gallery.ui/src/app/movies/details/[id]/page.tsx
@@ -1,4 +1,5 @@
 import { getMovieById } from "@/app/services/movie-service";
+import RatingStars from "@/app/ui/rating-stars";
 import Image from 'next/image';
 import styles from '@/app/ui/movie-details.module.css';
 
@@ -17,7 +18,7 @@ export default async function page({ params }: { params: { id: string } }) {
           />
         </div>
         <div className={styles.detailsFooter}>
-          <p>Year: 2000</p>
+          <p>{movie.year}</p>
           <p>{movie.classification}</p>
         </div>
       </div>
@@ -25,8 +26,16 @@ export default async function page({ params }: { params: { id: string } }) {
         <div className={styles.header}>
           <h1 className={styles.movieName}>{movie.name}</h1>
           <ul className={`${styles.classification} text-base`}>
-            <li><p className="text-slate-600 font-semibold">Duration: </p><p className="text-white">{movie.duration} min</p></li>
-            <li><p className="text-slate-600 font-semibold">Rating: </p>{movie.rating}</li>
+            <li>
+              <p className="text-slate-600 font-semibold">Duration:</p>
+              <p className="text-white">{movie.duration} minutes</p>
+            </li>
+            <li>
+              <p className="text-slate-600 font-semibold">Rating: </p>
+              <div className="flex w-16">
+                <RatingStars averageRating={Math.round(movie.rating)} />
+              </div>
+            </li>
           </ul>
         </div>
         <p className="text-md">{movie.synopsis}</p>
