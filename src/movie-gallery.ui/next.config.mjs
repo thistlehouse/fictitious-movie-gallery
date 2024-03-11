f755710d38a7d97ff9f79ffdb128a754c06bd8e5
thistlehouse <fernando2802@gmail.com>
2024-03-11 00:11:32 -0300

    using "remotePatterns"

--- src/movie-gallery.ui/next.config.mjs
+++ src/movie-gallery.ui/next.config.mjs
@@ -1,9 +1,11 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
-    domains: [
-      'placehold.co',
-      'localhost:5001',
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "**",
+      }
     ]
   }
 };
